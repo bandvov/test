f11c6bba8273b180c37a92eed4182c6194968371
Volodymyr Vasylkiv <bandvov@gmail.com>
2022-07-10 18:29:16 +0300

    add liqpay api

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, "."),
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
